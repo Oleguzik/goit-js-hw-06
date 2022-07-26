b556269c60ffd1cbed3e3d49c90cb306a46fe48e
Oleguzik <guzik.oleg@gmail.com>
2022-07-26 19:06:46 +0200

    task 1 ready

---
+++ js/task-01.js
@@ -0,0 +1,20 @@
+const listEl = document.querySelectorAll('#categories .item')
+console.log(`Number of categories: ${listEl.length}`)
+
+const listItem = document.querySelectorAll('h2')
+const elemUl = document.querySelectorAll('.item ul')
+// const itLi = []
+
+// elemUl.forEach(itemLi => {
+//   itLi.push(itemLi.childElementCount)
+// })
+
+// let i = 0
+
+listItem.forEach(itemL => {
+  console.log(`Category: ${itemL.textContent}`)
+  // console.log(`Elem: ${itLi[i]}`)
+  // const nextSN =
+  console.log(`Elem: ${itemL.nextElementSibling.childElementCount}`)
+  // i++
+})
