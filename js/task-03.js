fb5b7f0b13abb94b72ba99d12251df7a13cc632d
Oleguzik <guzik.oleg@gmail.com>
2022-07-28 22:51:48 +0200

    Task #3

--- js/task-03.js
+++ js/task-03.js
@@ -11,4 +11,20 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-];
+]
+const itemUl = document.querySelector('.gallery')
+itemUl.style.display = 'flex'
+itemUl.style.flexWrap = 'wrap'
+
+const fotoShadow =
+  'box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14), 0px 2px 1px rgba(0, 0, 0, 0.2);'
+
+const injectFoto = images
+  .map(
+    itemLi =>
+      `<li style='list-style: none; padding: 10px; ${fotoShadow}'><img width=250 src='${itemLi.url}' 
+      alt='${itemLi.alt}'></li>`
+  )
+  .join('')
+
+itemUl.insertAdjacentHTML('afterbegin', injectFoto)
