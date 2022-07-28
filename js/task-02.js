32ffc233b3712b8677c99fb191338b8d24b33b21
Oleguzik <guzik.oleg@gmail.com>
2022-07-28 20:26:26 +0200

    Task #2 reparation

--- js/task-02.js
+++ js/task-02.js
@@ -9,8 +9,19 @@ const ingredients = [
 
 const itemIngr = document.querySelector('#ingredients')
 
-const injectList = ingredients
-  .map(itemLi => `<li class="item">${itemLi}</li>`)
-  .join('')
+// const injectList = ingredients
+//   .map(itemLi => `<li class="item">${itemLi}</li>`)
+//   .join('')
+// itemIngr.insertAdjacentHTML('afterbegin', injectList)
 
-itemIngr.insertAdjacentHTML('afterbegin', injectList)
+// ingredients.forEach(itemLi => {
+//   itemIngr.innerHTML += `<li class="item">${itemLi}</li>`
+// })
+
+ingredients.forEach(itemLi => {
+  const itemL = document.createElement('li')
+  itemL.textContent = `${itemLi}`
+  itemL.style = 'item'
+
+  itemIngr.insertAdjacentHTML('beforeend', itemL.outerHTML)
+})
